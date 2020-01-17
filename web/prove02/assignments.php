f2e42861040ee49b10535083a6ad7204e8571c1a
Caleb Georgeson <geo16004@byui.edu>
2020-01-17 16:29:58 -0700

    prove 01 assignment stuff

---
+++ web/prove02/assignments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>
+         My Assignments
+      </title>
+      <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+      <script src="script.js"></script>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+   </head>
+   <body>
+      <header id="title-header" class="jumbotron"><h1>My Assignments</h1></header>
+      <?php echo "hello!"; ?>
+   </body>
+</html>
