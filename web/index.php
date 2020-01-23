7d42b8e657a8e2487b3a8e099bba3d158117b639
Caleb Georgeson <geo16004@byui.edu>
2020-01-23 09:02:52 -0700

    update index.php

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@
       <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
       <script src="script.js"></script>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="./prove02/style.css">
       <meta name="viewport" content="width=device-width, initial-scale=1">
    </head>
    <body>
