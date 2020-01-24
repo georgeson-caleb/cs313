fe29a814e54749f3c34e4d3fe79149af413d0b1f
Caleb Georgeson <geo16004@byui.edu>
2020-01-24 14:04:00 -0700

    fixed syntax error

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -1,15 +1,15 @@
 <?php
 
-   $item-list = json_decode($_POST["itemList"]);
-   $total-price = 0;
+   $itemList = json_decode($_POST["itemList"]);
+   $totalPrice = 0;
 
    foreach($item-list as $item) {
-      $img-name = $item->name . ".png";
+      $imgName = $item->name . ".png";
       $price = $item->price;
 
-      $total-price += floatval($price);
+      $totalPrice += floatval($price);
 
-      echo("<div class=''><img src=$img-name class=''><p></p><button class='btn btn-danger btn-block' onclick='removeFromCart($item->name)'>Remove from cart</button></div>");
+      echo("<div class=''><img src=$imgName class=''><p></p><button class='btn btn-danger btn-block' onclick='removeFromCart($item->name)'>Remove from cart</button></div>");
    }
 
    echo("<div class='d-flex'><button class='btn float-left btn-primary' onclick='goShopping()'>Continue shopping</button><button class='btn float-right btn-primary' onclick='checkout()'>Checkout</button></div>");
