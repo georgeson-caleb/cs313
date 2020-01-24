6b15d26d2b155a91ad6e944b8e8585e131d63128
Caleb Georgeson <geo16004@byui.edu>
2020-01-24 15:11:15 -0700

    fixed a typo

--- web/prove03/checkout.php
+++ web/prove03/checkout.php
@@ -4,7 +4,7 @@
       "<input class='form-control' type=text placeholder='Address Line 1' name=addressLine1>" . 
       "<input class='form-control' type=text placeholder='Address Line 2' name=addressLine2>" . 
       "<input class='form-control' type=text placeholder='City' name=city>" . 
-      "<input class='form-control' type=text placeholder='State' name=state>"
+      "<input class='form-control' type=text placeholder='State' name=state>" .
       "<input class='form-control' type=text placeholder='Zip Code' name=zip>" .
       "</div>");
 
