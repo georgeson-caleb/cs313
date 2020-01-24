b0aa494d9096ff9d95bb45ed6a3fe6a5e43e37af
Caleb Georgeson <geo16004@byui.edu>
2020-01-24 13:55:08 -0700

    added a semicolon

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -1,17 +1,17 @@
 <?php
 
-$item-list = json_decode($_POST["itemList"]);
-$total-price = 0;
+   $item-list = json_decode($_POST["itemList"]);
+   $total-price = 0;
 
-foreach($item-list as $item) {
-   $img-name = $item->name . ".png";
-   $price = $item->price;
+   foreach($item-list as $item) {
+      $img-name = $item->name . ".png";
+      $price = $item->price;
 
-   $total-price += floatval($price);
+      $total-price += floatval($price);
 
-   echo("<div class=''><img src=$img-name class=''><p></p><button class='btn btn-danger btn-block' onclick='removeFromCart($item->name)'>Remove from cart</button></div>");
-}
+      echo("<div class=''><img src=$img-name class=''><p></p><button class='btn btn-danger btn-block' onclick='removeFromCart($item->name)'>Remove from cart</button></div>");
+   }
 
-echo("<div class='d-flex'><button class='btn float-left btn-primary' onclick='goShopping()'>Continue shopping</button><button class='btn float-right btn-primary' onclick='checkout()'>Checkout</button></div>")
+   echo("<div class='d-flex'><button class='btn float-left btn-primary' onclick='goShopping()'>Continue shopping</button><button class='btn float-right btn-primary' onclick='checkout()'>Checkout</button></div>");
 
 ?>
