b4640d601d5e495622d0815585be4cee1c0c31ee
Caleb Georgeson <geo16004@byui.edu>
2020-01-24 12:37:45 -0700

    added prices and centered elements

--- web/prove03/index.php
+++ web/prove03/index.php
@@ -14,11 +14,18 @@
             Miscellaneous Stuff For Sale
 </h1>
 </header>
-         <div class="d-flex flex-wrap">
+         <div class="d-flex flex-wrap mx-auto">
             <?php
                $images = array("Bananas", "Bike", "Bottles", "Laptop", "Dog");
+               $prices = array(
+                  "Bananas" => "1.98",
+                  "Bike" => "245"
+                  "Bottles" => "3.50"
+                  "Laptop" => "499"
+                  "Dog" => "299"
+               )
                foreach ($images as $image) {
-                  echo "<div class='m-1 p-2 w-25 border rounded'><img src='$image.png' class='img-fluid'><p>$image</p><button class='btn btn-block btn-primary' onclick='addToCart($image)'>Add to cart</button></div>";
+                  echo "<div class='my-1 mx-auto p-2 w-25 border rounded'><img src='$image.png' class='img-fluid'><p>$image </br> $prices[$image]</p><button class='btn btn-block btn-primary' onclick='addToCart($image, $prices[$image]'>Add to cart</button></div>";
                }
             ?>
 </div>
