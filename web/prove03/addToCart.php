b59fbc138fdb11f389cd2e63a8b66575263e6389
Caleb Georgeson <geo16004@byui.edu>
2020-01-25 09:00:42 -0700

    trying something else

--- web/prove03/addToCart.php
+++ web/prove03/addToCart.php
@@ -1,6 +1,9 @@
 <?php 
-
-   array_push($_SESSION["cart"], json_decode($_POST["item"]));
+   if ($_SESSION["cart"] == null) {
+      $_SESSION["cart"] = array(json_decode($_POST["item"]));
+   } else {
+      array_push($_SESSION["cart"], json_decode($_POST["item"]));
+   }
 
    echo(json_encode($_SESSION["cart"]));
 
