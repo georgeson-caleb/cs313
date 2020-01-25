a736012a6f0739b8e84acc79b74f6f84784d6e2c
Caleb Georgeson <geo16004@byui.edu>
2020-01-25 10:26:43 -0700

    cal;dcnqec

--- web/prove03/removeFromCart.php
+++ web/prove03/removeFromCart.php
@@ -9,7 +9,7 @@
       } 
    }
    
-   $_SESSION["totalPrice"] -= $_SESSION["cart"][$i]->price;
+   $_SESSION["totalPrice"] -= floatval($_SESSION["cart"][$i]->price);
 
    echo($_SESSION["totalPrice"]);
 
