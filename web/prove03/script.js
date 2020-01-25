b462318b6706ea6300a7c92426709c9298e4c93e
Caleb Georgeson <geo16004@byui.edu>
2020-01-25 09:22:41 -0700

    fixed a typo

--- web/prove03/script.js
+++ web/prove03/script.js
@@ -151,7 +151,7 @@ function deleteFromSession(data) {
    xhttp.send("item=" + data);
 }
 
-function getTotalprice() {
+function getTotalPrice() {
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
