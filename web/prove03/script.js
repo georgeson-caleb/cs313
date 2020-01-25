44b68bdfbf1b8fe5d8b6a5575ea022dcaca30f7e
Caleb Georgeson <geo16004@byui.edu>
2020-01-24 20:46:04 -0700

    added form validation

--- web/prove03/script.js
+++ web/prove03/script.js
@@ -58,3 +58,34 @@ function checkout() {
    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
    xhttp.send();
 }
+
+function confirmPurchase() {
+   // Check the user's input
+   var valid = true;
+   var elemList = [];
+   elemList.add(document.getElementById("addressLine1"));
+   elemList.add(document.getElementById("addressLine2"));
+   elemList.add(document.getElementById("city"));
+   elemList.add(document.getElementById("state"));
+   elemList.add(document.getElementById("zip"));
+
+   for (var i = 0; i < elemList.length; i++) {
+      // Address Line 2 is not required
+      if (i != 1) {
+         if (elemList[i].value != "") {
+            // valid stays true unless it has already been set to false
+            valid = valid & true;
+         } else {
+            // Set valid to false
+            valid = false;
+            // Show that the box is invalid
+            elemList[i].classList.add("border border-danger");
+         }
+      }
+
+      if (valid) {
+         //Go to confirmation page
+      }
+   }
+   
+}
