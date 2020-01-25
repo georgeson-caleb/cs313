ed8ab2bae6bdd656d2f04f98f6e9f345671d1259
Caleb Georgeson <geo16004@byui.edu>
2020-01-25 09:41:25 -0700

    trying something new

--- web/prove03/removeFromCart.php
+++ web/prove03/removeFromCart.php
@@ -2,12 +2,17 @@
    session_start();
 
    $index;
+   $totalPrice = 0;
 
    for ($i = 0; $i < count($_SESSION["cart"]); $i++) {
       if ($_SESSION["cart"][i]->name == $_POST["item"]) {
          $index = $i;
+      } else {
+         $totalPrice += $_SESSION["cart"][i]->price;
       }
    }
    
    array_splice($_SESSION["cart"], $index);
+
+   echo $totalPrice;
 ?>
