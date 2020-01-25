1f1b8d7f1b06aaffe3268cb9ff90e140e9bfe9a7
Caleb Georgeson <geo16004@byui.edu>
2020-01-25 09:08:59 -0700

    this should work

--- web/prove03/removeFromCart.php
+++ web/prove03/removeFromCart.php
@@ -1,7 +1,7 @@
 <?php
    session_start();
    
-   $index = array_search($_POST["item"], $_SESSION["cart"]);
+   $index = array_search(json_decode($_POST["item"]), $_SESSION["cart"]);
    array_splice($_SESSION["cart"], $index);
 
    echo(json_encode($_SESSION["cart"]));
