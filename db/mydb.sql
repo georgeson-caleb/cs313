de2fcd74ea314619a7369c4b0bb657ca219dd9c1
Caleb Georgeson <geo16004@byui.edu>
2020-01-27 19:46:30 -0700

    added database and initial page

---
+++ db/mydb.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+   id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+   username varchar(255) NOT NULL,
+   password varchar(255) NOT NULL,
+   first_name varchar(255) NOT NULL,
+   last_name varchar(255) NOT NULL
+);
+
+CREATE TABLE cats (
+   id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+   name varchar(255) NOT NULL,
+   fav_food varchar(255),
+   fav_pastime varchar(255),
+   age int,
+   owner_id int REFERENCES users (id)
+);
+
+CREATE TABLE pictures (
+   id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+   image_name varchar(255) NOT NULL,
+   comments TEXT[],
+   likes int,
+   cat_id int REFERENCES cats (id)
+);
---
+++ web/catscratch/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+   <title>Catscratch - Share Your Kitties</title>
+   <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+   <script src="script.js"></script>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+   <link rel="stylesheet" href="./prove02/style.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<header class="jumbotron">
+   <h1>Catscratch</h1>
+</header>
+</body>
+</html>
