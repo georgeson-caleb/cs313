e259a4c800a839b5ed8cbee85d17b2324945155b
Caleb Georgeson <geo16004@byui.edu>
2020-01-31 13:59:42 -0700

    testing signup

--- web/catscratch/script.js
+++ web/catscratch/script.js
@@ -56,7 +56,7 @@ function submitSignupInfo(username, email, password) {
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-         //Go to homepage
+         console.log(this.responseText);
       }
    }
 
