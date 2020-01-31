7441119eae85dc6d2a043c8a288c80255d99408e
Caleb Georgeson <geo16004@byui.edu>
2020-01-31 14:19:13 -0700

    testing php

--- web/catscratch/submitSignup.php
+++ web/catscratch/submitSignup.php
@@ -2,21 +2,18 @@
 
    require("dbConnect.php");
 
-   # $username = strip_tags($_POST["username"]);
-   # $email = strip_tags($_POST["email"]);
-   # $password = strip_tags($_POST["password"]);
-
-   $username = "username";
-   $email = "name@example.com";
-   $password = "password";
+   $username = strip_tags($_POST["username"]);
+   $email = strip_tags($_POST["email"]);
+   $password = strip_tags($_POST["password"]);
 
 
    $tater = password_hash($password, PASSWORD_BCRYPT);
 
    $db = get_db();
 
-   $query = "INSERT INTO users (username, email, pass) VALUES ($username, $email, $tater);";
+   $query = "INSERT INTO users (username, email, pass) VALUES (:username, :email, :tater);";
 
    $stmt = $db->prepare($query);
+   $stmt->bindValue(':username', $username, )
    $stmt->execute();
 ?>
