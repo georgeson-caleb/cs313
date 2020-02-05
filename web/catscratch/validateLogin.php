dd1e5466c67699c7215303b93e33cd074c266b07
Caleb Georgeson <geo16004@byui.edu>
2020-02-05 13:18:59 -0700

    testing

--- web/catscratch/validateLogin.php
+++ web/catscratch/validateLogin.php
@@ -1,21 +1,18 @@
 <?php 
-   //require("dbConnect.php");
-   //$db = get_db();
-//
-   //$username = strip_tags($_POST["username"]);
-   //$password = strip_tags($_POST["password"]);
-   //$query = "SELECT pass FROM users WHERE username=:username";
-   //$stmt = $db->prepare($query);
-   //$stmt->bindValue(":username", $username, PDO::PARAM_STR);
-   //$stmt->execute();
-//
-   //$hash = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   require("dbConnect.php");
+   $db = get_db();
 
-   echo("is this working?");
+   $username = strip_tags($_POST["username"]);
+   $password = strip_tags($_POST["password"]);
+   $query = "SELECT pass FROM users WHERE username=:username";
+   $stmt = $db->prepare($query);
+   $stmt->bindValue(":username", $username, PDO::PARAM_STR);
+   $stmt->execute();
+
+   $hash = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
    //echo(password_verify($password[0], $hash));
 
-   /*
    if (count($password) > 1) {
       #something is wrong. there should only be 1
    } else if (count($password) == 0) {
@@ -30,5 +27,5 @@
          echo "invalid credentials";
       }
    }
-   */
+   
 ?>
