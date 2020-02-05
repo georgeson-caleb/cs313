168a9f708e37803658571177c1b44c8a27515614
Caleb Georgeson <geo16004@byui.edu>
2020-02-05 13:15:54 -0700

    testing

--- web/catscratch/script.js
+++ web/catscratch/script.js
@@ -90,7 +90,7 @@ function login() {
 
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
-      if (this.readystate == 4 && this.status == 200) {
+      if (this.readyState == 4 && this.status == 200) {
          processLogin(this.responseText);
       }
    }
