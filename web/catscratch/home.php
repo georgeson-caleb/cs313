e1dec76121330d5c681a1bcc1514c14d06ec3882
Caleb Georgeson <geo16004@byui.edu>
2020-02-05 14:41:59 -0700

    testing

--- web/catscratch/home.php
+++ web/catscratch/home.php
@@ -1,3 +1,21 @@
 <?php
+   session_start();
    echo "logged in";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title> Catscratch - Share your kitties!</title>
+</head>
+<body>
+   <h1>Catscratch</h1>
+   <?php
+      if ($_SESSION["dq4r1"] == "") {
+         echo "not logged id";
+      } else {
+         echo "logged in";
+      }
+   ?>
+</body>
+</html>
