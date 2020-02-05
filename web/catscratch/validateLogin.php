66dd07cb539256539bce1d48d6a239113ea7d974
Caleb Georgeson <geo16004@byui.edu>
2020-02-05 13:37:18 -0700

    testing

--- web/catscratch/validateLogin.php
+++ web/catscratch/validateLogin.php
@@ -20,7 +20,7 @@
       echo "something wrong";
    } else {
       # check the password
-      if (password_verify($password, $hash[0])) {
+      if (password_verify($password, $hash)) {
          echo "logging in...";
       } else {
          # send password error message
