bff4df69615f32d5cf8b6338a0aef2728ff31feb
Caleb Georgeson <geo16004@byui.edu>
2020-02-05 14:36:28 -0700

    testing

--- web/catscratch/validateLogin.php
+++ web/catscratch/validateLogin.php
@@ -1,6 +1,5 @@
 <?php 
    require("dbConnect.php");
-   require("getUserId.php");
 
    session_start(); 
 
@@ -8,14 +7,12 @@
 
    $username = strip_tags($_POST["username"]);
    $password = strip_tags($_POST["password"]);
-   $query = "SELECT pass FROM users WHERE username=:username";
+   $query = "SELECT pass, id FROM users WHERE username=:username";
    $stmt = $db->prepare($query);
    $stmt->bindValue(":username", $username, PDO::PARAM_STR);
    $stmt->execute();
 
-   $hash = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-   echo json_encode($hash);
+   $userInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
    if (count($hash) > 1) {
       #something is wrong. there should only be 1
@@ -24,8 +21,8 @@
       echo "no";
    } else {
       # check the password
-      if (password_verify($password, $hash[0]["pass"])) {
-        $_SESSION["dq4r1"] = getUserId($username, $db);
+      if (password_verify($password, $userInfo[0]["pass"])) {
+        $_SESSION["dq4r1"] = $userInfo[0]["id"];
          ob_clean();
          header("Location: home.php");
          exit();
