777326e93873b08802c1c91d937faa4b50191d5c
Caleb Georgeson <geo16004@byui.edu>
2020-02-15 08:55:18 -0700

    Adding extra prompts to add-cat.php

--- web/catscratch/upload.php
+++ web/catscratch/upload.php
@@ -48,11 +48,15 @@
 
          echo "File upload successful! <img src=$filename class='img-fluid w-25'>";
          $cat_name = strip_tags($_POST["name"]);
+         $fav_food = strip_tags($_POST["food"]);
+         $fav_pastime = strip_tags($_POST["pastime"]);
          $age = strip_tags($_POST["age"]);
          $db = get_db();
-         $query = "INSERT INTO cats (cat_name, age, owner_id) VALUES (:cat_name, :age, :id);";
+         $query = "INSERT INTO cats (cat_name, fav_food, fav_pastime,  age, owner_id) VALUES (:cat_name, :fav_food, :fav_pastime, :age, :id);";
          $stmt = $db->prepare($query);
          $stmt->bindValue(":cat_name", $cat_name, PDO::PARAM_STR);
+         $stmt->bindValue(":fav_food", $fav_food, PDO::PARAM_STR);
+         $stmt->bindValue(":fav_pastime", $fav_pastime, PDO::PARAM_STR);
          $stmt->bindValue(":age", $age, PDO::PARAM_INT);
          $stmt->bindValue(":id", $_SESSION["dq4r1"], PDO::PARAM_INT);
          $stmt->execute();
