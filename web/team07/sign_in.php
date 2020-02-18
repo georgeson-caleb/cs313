a58e90ce0f98789a9e46ac8d71bafaaae44a2547
Caleb Georgeson <geo16004@byui.edu>
2020-02-18 09:32:24 -0700

    team07 activity

--- web/team07/sign_in.php
+++ web/team07/sign_in.php
@@ -2,6 +2,7 @@
 <html lang="en-US">
 <head>
 <title>Welcome!</title>
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
 <script src="sign_in_verify.js"></script>
 <head>
 <body>
@@ -11,6 +12,7 @@
 <br><br>
 
 <div id="login">
+   <div style="display:none; color:red;" id="error"></div>
    <input type="text" id="username" name="username" placeholder="Username">
    <input type="password" id="password" name="password" placeholder="Password">
    <button type="button" onclick="submitInfo()">Sign in!</button>
