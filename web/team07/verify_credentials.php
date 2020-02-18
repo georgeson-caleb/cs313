64fc6b2a6ff42b921e6c6cf3bbe62f949046f4c6
Caleb Georgeson <geo16004@byui.edu>
2020-02-18 10:24:09 -0700

    team07 activity

--- web/team07/verify_credentials.php
+++ web/team07/verify_credentials.php
@@ -6,14 +6,14 @@ $db = get_db();
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
-$stmt = $db->prepare("SELECT user_id, password FROM users_team WHERE username=:username");
+$stmt = $db->prepare("SELECT user_id, password FROM users_team WHERE username=:username LIMIT 1");
 $stmt->bindValue(":username", $username, PDO::PARAM_STR);
 $stmt->execute();
 
-$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
 echo(json_encode($row));
-$id = $row[0]["user_id"];
-$password_hash = $row[0]["password"];
+$id = $row["user_id"];
+$password_hash = $row["password"];
 
 if(password_verify($password, $password_hash)) {
     $_SESSION['loggedin'] = TRUE;
