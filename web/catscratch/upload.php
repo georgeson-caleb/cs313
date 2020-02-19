7b5a73a02272a9c7939348c5c99035e4b1aea751
Caleb Georgeson <geo16004@byui.edu>
2020-02-19 12:35:46 -0700

    testing aws upload

--- web/catscratch/upload.php
+++ web/catscratch/upload.php
@@ -1,10 +1,15 @@
 <?php
 
    require("dbConnect.php");
+   require('../../vendor/autoload.php');
+
+   $s3 = new Aws\S3\S3Client([
+   'version' => '2006-03-01',
+   'region'  => 'us-west-2'
+   ]);
 
    session_start();
 
-   echo $_SESSION["dq4r1"] . "<br>";
    $path = "img/";
 
    if ($_SESSION["dq4r1"] == "") {
@@ -12,8 +17,6 @@
    } else {
       $path .= $_SESSION["dq4r1"] . "/";
    }
-
-   echo $path . "<br>";
 
    // Check if the path exists. If not, create it
    if (!is_dir($path)) {
@@ -45,34 +48,38 @@
 
 
    if ($uploadOk) {
-      if (move_uploaded_file($_FILES["image"]["tmp_name"], $filename)) {
 
-         echo "File upload successful!  src=$filename class='img-fluid w-25'>";
-         $cat_name = strip_tags($_POST["name"]);
-         $fav_food = strip_tags($_POST["food"]);
-         $fav_pastime = strip_tags($_POST["pastime"]);
-         $age = strip_tags($_POST["age"]);
-         $db = get_db();
-         $query = "INSERT INTO cats (cat_name, fav_food, fav_pastime,  age, owner_id) VALUES (:cat_name, :fav_food, :fav_pastime, :age, :id);";
-         $stmt = $db->prepare($query);
-         $stmt->bindValue(":cat_name", $cat_name, PDO::PARAM_STR);
-         $stmt->bindValue(":fav_food", $fav_food, PDO::PARAM_STR);
-         $stmt->bindValue(":fav_pastime", $fav_pastime, PDO::PARAM_STR);
-         $stmt->bindValue(":age", $age, PDO::PARAM_INT);
-         $stmt->bindValue(":id", $_SESSION["dq4r1"], PDO::PARAM_INT);
-         $stmt->execute();
-
-         $cat_id = $db->lastInsertId();
-
-         $query = "INSERT INTO pictures (image_name, cat_id) VALUES (:image_name, :cat_id);";
-         $stmt = $db->prepare($query);
-         $stmt->bindValue(":image_name", $filename, PDO::PARAM_STR);
-         $stmt->bindValue(":cat_id", $cat_id, PDO::PARAM_INT);
-         $stmt->execute();
-
-      } else {
-         echo "There was an error uploading the file.";
+      $bucket = getenv('S3_BUCKET')? : die("Couldn't get the bucket");
+      
+      try {
+         $upload = $s3->upload($bucket, $filename, fopen($_FILES["image"]["tmp_name"], 'rb'), 'public-read'); 
+      } catch(Exception $e) {
+         echo "Error uploading file";
+         die();
       }
+
+      $cat_name = strip_tags($_POST["name"]);
+      $fav_food = strip_tags($_POST["food"]);
+      $fav_pastime = strip_tags($_POST["pastime"]);
+      $age = strip_tags($_POST["age"]);
+      $db = get_db();
+      $query = "INSERT INTO cats (cat_name, fav_food, fav_pastime, age, owner_id) VALUES (:cat_name, :fav_food, :fav_pastime, :age, :id);";
+      $stmt = $db->prepare($query);
+      $stmt->bindValue(":cat_name", $cat_name, PDO::PARAM_STR);
+      $stmt->bindValue(":fav_food", $fav_food, PDO::PARAM_STR);
+      $stmt->bindValue(":fav_pastime", $fav_pastime, PDO::PARAM_STR);
+      $stmt->bindValue(":age", $age, PDO::PARAM_INT);
+      $stmt->bindValue(":id", $_SESSION["dq4r1"], PDO::PARAM_INT);
+      $stmt->execute();
+
+      $cat_id = $db->lastInsertId();
+
+      $query = "INSERT INTO pictures (image_name, cat_id) VALUES (:image_name, :cat_id);";
+      $stmt = $db->prepare($query);
+      $stmt->bindValue(":image_name", $upload->get("ObjectURL"), PDO::PARAM_STR);
+      $stmt->bindValue(":cat_id", $cat_id, PDO::PARAM_INT);
+      $stmt->execute();
+
    } else {
       echo "The file was not uploaded.";
    }
