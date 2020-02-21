7f6427e0906ae395dc18bd02256095c243242746
Caleb Georgeson <geo16004@byui.edu>
2020-02-21 13:09:15 -0700

    fixed syntax error

--- web/catscratch/showCats.php
+++ web/catscratch/showCats.php
@@ -1,4 +1,5 @@
 <?php
+
    function showCats($userId) {
 
       // Get the ids of any cats associated with the user id
@@ -20,11 +21,9 @@
          $stmt->execute();
          array_push($pictures, $stmt->fetchAll(PDO::FETCH_ASSOC));
       }
-?>
 
-   <h3 class="text-center">Your Cats</h3>
-   <div id="info-box" class="d-flex flex-wrap w-75 mx-auto mb-2 p-2 border rounded">
-      <?
+   echo'<h3 class="text-center">Your Cats</h3><div id="info-box" class="d-flex flex-wrap w-75 mx-auto mb-2 p-2 border rounded">';
+
 
          if (count($cats) == 0) {
             echo "No cats yet. Add some to see them here!";
@@ -32,13 +31,12 @@
 
          foreach ($pictures as $picture) {
             $image = $picture[0]["image_name"];
-            $cat_name = getCatName($picture[0]["cat_id"]);
-            ?> 
-               <div class='border rounded w-25 mx-2 mb-3'>
+            $cat_name = getCatName($picture[0]["cat_id"]); 
+              echo "<div class='border rounded w-25 mx-2 mb-3'>
                   <h3 class='text-center'>$cat_name</h3>
                   <img src='$image' class='img-fluid'>
-               </div>
-            <?
+               </div>";
+
          }
    }
 
@@ -51,4 +49,5 @@
 
       return $stmt->fetch(PDO::FETCH_ASSOC)["cat_name"];
    }
+
 ?>
