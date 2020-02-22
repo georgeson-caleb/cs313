3a4fe1d45bc7cf0b760cc3af2cefae568a199052
Caleb Georgeson <geo16004@byui.edu>
2020-02-22 11:35:02 -0700

    checking picture

--- web/catscratch/home.php
+++ web/catscratch/home.php
@@ -87,8 +87,10 @@
          $stmt->execute();
          $pics = $stmt->fetchAll(PDO::FETCH_ASSOC);
          if ($stmt->rowCount() > 0) {
+            echo("Pushing picture");
             array_push($pictures, $pics);
          } else {
+            echo("Pushing pixel cat");
             $pictureStandIn = array();
             arrayPush($pictureStandIn, array("image_name" => "/img/pixel_cat.png", "cat_id" => $cat["id"]));
             array_push($pictures, $pictureStandIn);
