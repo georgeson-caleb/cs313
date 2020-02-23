d7c312dedef38567e3fe8ff40cc6d217ce59bcda
Caleb Georgeson <geo16004@byui.edu>
2020-02-22 23:15:14 -0700

    testing

--- web/catscratch/home.php
+++ web/catscratch/home.php
@@ -108,7 +108,7 @@ if (count($cats) == 0) {
 foreach ($pictures as $picture) {
    $image = $picture["image_name"];
    $cat_name = getCatName($picture["cat_id"]); 
-     echo "<div class='cat-card border rounded w-25 mx-2 mb-3 view overlay zoom'>
+     echo "<div class='cat-card border rounded w-25 mx-2 mb-3'>
          <h3 class='text-center'>$cat_name</h3>
          <img src='$image' class='img-fluid w-100'>
       </div>";
